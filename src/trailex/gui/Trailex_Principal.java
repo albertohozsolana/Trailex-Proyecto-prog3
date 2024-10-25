be758c919824bca4fdfe2d04f6a1506edef00ccd
Lucía <lucia.diez@opendeusto.es>
2024-10-25 19:58:31 +0200

    Series organizadas por géneros

--- src/trailex/gui/Trailex_Principal.java
+++ src/trailex/gui/Trailex_Principal.java
@@ -8,6 +8,7 @@ import java.awt.GridLayout;
 import java.awt.Image;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -16,6 +17,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
 
 import trailex.elementos.Pelicula;
@@ -27,8 +30,12 @@ public class Trailex_Principal extends JFrame{
 	private JPanel panel_central;
 	private JPanel panel_principal;
 	private JPanel panel_arriba;
+	private JPanel panel_comedia, panel_romance, panel_aventura, panel_drama,panel_cf, panel_terror;
 	private JButton boton_menu, boton_pelis, boton_series;
 	private Color turquesa=new Color(0x5FA6AD);
+	private JLabel t_comedia, t_romance, t_aventura, t_drama, t_cf, t_terror;
+	private JPanel p_norte_comedia, p_grid_comedia, p_norte_romance, p_grid_romance, p_norte_cf, p_norte_terror, p_norte_drama, p_norte_aventura, p_grid_aventura, p_grid_drama, p_grid_cf, p_grid_terror;
+	private GridLayout grid_comedia, grid_romance, grid_cf, grid_terror, grid_drama, grid_aventura;
 	
 	private static final long serialVersionUID = 1L;
 
@@ -71,7 +78,9 @@ public class Trailex_Principal extends JFrame{
 				
 				// Creamos y añadimos un Desplegable a la parte de arriba
 				String[] generos = {"Comedia", "Terror", "Romantica", "Ciencia Ficción","Aventura","Drama"};
-				JComboBox<String> lista_desplegable = new JComboBox<String>(generos);	
+				JComboBox<String> lista_desplegable = new JComboBox<String>(generos);
+				
+				lista_desplegable.setSelectedIndex(-1);
 				lista_desplegable.setToolTipText("Selecciona un género");
 				lista_desplegable.setBackground(new Color(0x5FA6AD));
 				panel_arriba.add(lista_desplegable);
@@ -87,15 +96,129 @@ public class Trailex_Principal extends JFrame{
 
 				
 				// Añadimos a la pantalla principal la parte central (CON UN BOTÓN) y le decimos que queremos que esté en el centro
-				panel_central= new JPanel(new GridLayout(0,7));
+				panel_central= new JPanel(new GridLayout(6,1));
 				panel_central.setBackground(Color.black);
 
-				cargarSeries();
-				/*
-				JScrollPane scroll= new JScrollPane(panel_central);
-				panel_principal.add(scroll, BorderLayout.CENTER);
-				*/
+				
+				
+				
 
+				//ORGANIZAMOS LAS SERIES EN FILAS POR GENERO-----------------------------------------------------
+				
+				//COMEDIA
+				panel_comedia = new JPanel();
+				
+				panel_comedia.setLayout(new BorderLayout());
+				
+				grid_comedia= new GridLayout(1,0);
+				
+				p_grid_comedia= new JPanel();
+				p_grid_comedia.setLayout(grid_comedia);
+				JScrollPane scroll_comedia= new JScrollPane(p_grid_comedia, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+				scroll_comedia.getHorizontalScrollBar().setAlignmentX(LEFT_ALIGNMENT);
+
+				
+				t_comedia = new JLabel("Comedia");
+				p_norte_comedia = new JPanel(new FlowLayout(FlowLayout.LEFT));
+				p_norte_comedia.add(t_comedia);
+								
+				panel_comedia.add(p_norte_comedia, BorderLayout.NORTH);
+				//panel_comedia.add(p_grid_comedia, BorderLayout.CENTER);
+				panel_comedia.add(scroll_comedia, BorderLayout.CENTER);
+
+				panel_central.add(panel_comedia);
+					
+				
+				// ROMANCE
+				panel_romance = new JPanel();
+				panel_romance.setLayout(new BorderLayout());
+
+				grid_romance = new GridLayout(1, 0);
+				p_grid_romance = new JPanel();
+				p_grid_romance.setLayout(grid_romance);
+
+				t_romance = new JLabel("Romance"); // Cambiado a t_romance
+				p_norte_romance = new JPanel(new FlowLayout(FlowLayout.LEFT));
+				p_norte_romance.add(t_romance);
+
+				panel_romance.add(p_norte_romance, BorderLayout.NORTH);
+				panel_romance.add(p_grid_romance, BorderLayout.CENTER); // Usar CENTER para el grid
+				panel_central.add(panel_romance);
+				
+
+				// AVENTURA
+				panel_aventura = new JPanel();
+				panel_aventura.setLayout(new BorderLayout());
+
+				grid_aventura = new GridLayout(1, 0);
+				p_grid_aventura = new JPanel();
+				p_grid_aventura.setLayout(grid_aventura);
+
+				t_aventura = new JLabel("Aventura");
+				p_norte_aventura = new JPanel(new FlowLayout(FlowLayout.LEFT));
+				p_norte_aventura.add(t_aventura);
+
+				panel_aventura.add(p_norte_aventura, BorderLayout.NORTH);
+				panel_aventura.add(p_grid_aventura, BorderLayout.CENTER); // Usar CENTER para el grid
+				panel_central.add(panel_aventura);
+				
+
+				// DRAMA
+				panel_drama = new JPanel();
+				panel_drama.setLayout(new BorderLayout());
+
+				grid_drama = new GridLayout(1, 0);
+				p_grid_drama = new JPanel();
+				p_grid_drama.setLayout(grid_drama);
+
+				t_drama = new JLabel("Drama");
+				p_norte_drama = new JPanel(new FlowLayout(FlowLayout.LEFT));
+				p_norte_drama.add(t_drama);
+
+				panel_drama.add(p_norte_drama, BorderLayout.NORTH);
+				panel_drama.add(p_grid_drama, BorderLayout.CENTER); // Usar CENTER para el grid
+				panel_central.add(panel_drama);
+				
+
+				// CIENCIA FICCIÓN
+				panel_cf = new JPanel();
+				panel_cf.setLayout(new BorderLayout());
+
+				grid_cf = new GridLayout(1, 0);
+				p_grid_cf = new JPanel();
+				p_grid_cf.setLayout(grid_cf);
+
+				t_cf = new JLabel("Ciencia Ficción");
+				p_norte_cf = new JPanel(new FlowLayout(FlowLayout.LEFT));
+				p_norte_cf.add(t_cf);
+
+				panel_cf.add(p_norte_cf, BorderLayout.NORTH);
+				panel_cf.add(p_grid_cf, BorderLayout.CENTER); // Usar CENTER para el grid
+				panel_central.add(panel_cf);
+
+				
+				// TERROR
+				panel_terror = new JPanel();
+				panel_terror.setLayout(new BorderLayout());
+
+				grid_terror = new GridLayout(1, 0);
+				p_grid_terror = new JPanel();
+				p_grid_terror.setLayout(grid_terror);
+
+				t_terror = new JLabel("Terror");
+				p_norte_terror = new JPanel(new FlowLayout(FlowLayout.LEFT));
+				p_norte_terror.add(t_terror);
+
+				panel_terror.add(p_norte_terror, BorderLayout.NORTH);
+				panel_terror.add(p_grid_terror, BorderLayout.CENTER); // Usar CENTER para el grid
+				panel_central.add(panel_terror);
+				
+				
+
+
+				cargarSeries();
+				
+				
 				
 				// Configuramos el panel principal
 				this.add(panel_principal);
@@ -106,7 +229,7 @@ public class Trailex_Principal extends JFrame{
 				Image img_icono= img.getImage();
 				this.setIconImage(img_icono);
 				
-				JScrollPane scroll= new JScrollPane(panel_central);
+				JScrollPane scroll= new JScrollPane(panel_central, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 				panel_principal.add(scroll, BorderLayout.CENTER);
 				this.setVisible(true);
 	}
@@ -125,7 +248,15 @@ public class Trailex_Principal extends JFrame{
 			Border border=BorderFactory.createLineBorder(turquesa,2);
 			lblFoto.setBorder(border);
 			
-			panel_central.add(lblFoto);
+			p_grid_comedia.add(lblFoto);
+			
+			/*
+			p_grid_romance.add(lblFoto);
+			p_grid_drama.add(lblFoto);
+			p_grid_aventura.add(lblFoto);
+			p_grid_terror.add(lblFoto);
+			p_grid_cf.add(lblFoto);
+			*/
 		}
 		panel_central.revalidate();
 		panel_central.repaint();
