c3a142916b1eac7d543d9501dd72e1cea29ff933
Luis Coro <luis.coro@opendeusto.es>
2025-01-20 00:50:09 +0100

    parte2

---
+++ src/trailex/gui/ThemeManager.java
@@ -0,0 +1,30 @@
+package trailex.gui;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import trailex.gui.Trailex_Principal.Tema;
+
+public class ThemeManager {
+    private static final String THEME_FILE = "theme.txt";
+
+    public static Tema cargarTema() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(THEME_FILE))) {
+            String theme = reader.readLine();
+            return Tema.valueOf(theme.toUpperCase());
+        } catch (IOException | IllegalArgumentException e) {
+            return Tema.BLANCO; // Valor predeterminado si no se encuentra el archivo o hay un error
+        }
+    }
+
+    public static void guardarTema(Tema tema) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(THEME_FILE))) {
+            writer.write(tema.name());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
